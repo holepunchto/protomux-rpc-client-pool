2ea1e181a820cd93b815623c7078fc0f0f6c3240
Thang Nguyen <1080481+vietthang@users.noreply.github.com>
2025-12-16 15:42:04 +0700

    migrate to prettier (#3)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const b4a = require('b4a')
 const PoolError = require('./lib/errors')
 
 class ProtomuxRpcClientPool {
-  constructor (keys, rpcClient, { retries = 3, timeout = 3000 } = {}) {
+  constructor(keys, rpcClient, { retries = 3, timeout = 3000 } = {}) {
     // TODO: ensure failover is to a random key too (for example by random-sorting the keys when passed-in)
     this.keys = keys.map(IdEnc.decode)
     this.statelessRpc = rpcClient
@@ -12,24 +12,28 @@ class ProtomuxRpcClientPool {
     this.chosenKey = pickRandom(this.keys)
   }
 
-  async makeRequest (methodName, args, { requestEncoding, responseEncoding, timeout } = {}) {
+  async makeRequest(methodName, args, { requestEncoding, responseEncoding, timeout } = {}) {
     timeout = timeout || this.timeout
 
     let key = this.chosenKey
     for (let i = 0; i < this.retries; i++) {
       try {
-        return await this.statelessRpc.makeRequest(
-          key,
-          methodName,
-          args,
-          { timeout, requestEncoding, responseEncoding }
-        )
+        return await this.statelessRpc.makeRequest(key, methodName, args, {
+          timeout,
+          requestEncoding,
+          responseEncoding
+        })
       } catch (e) {
         // TODO: figure out other errors that should result in a retry
-        if (e.code === 'REQUEST_TIMEOUT' || e.code === 'CHANNEL_CLOSED' || e.code === 'TIMEOUT_EXCEEDED') {
+        if (
+          e.code === 'REQUEST_TIMEOUT' ||
+          e.code === 'CHANNEL_CLOSED' ||
+          e.code === 'TIMEOUT_EXCEEDED'
+        ) {
           if (b4a.equals(key, this.chosenKey)) {
             this.chosenKey = key = pickNext(this.keys, key)
-          } else { // Some other request already rotated the key
+          } else {
+            // Some other request already rotated the key
             key = this.chosenKey
           }
           continue
@@ -42,11 +46,11 @@ class ProtomuxRpcClientPool {
   }
 }
 
-function pickRandom (keys) {
+function pickRandom(keys) {
   return keys[Math.floor(Math.random() * keys.length)]
 }
 
-function pickNext (keys, key) {
+function pickNext(keys, key) {
   let foundI = 0
   for (let i = 0; i < keys.length; i++) {
     if (b4a.equals(keys[i], key)) {
--- test.js
+++ test.js
@@ -10,7 +10,7 @@ const Pool = require('.')
 
 const DEBUG = true
 
-test('happy path', async t => {
+test('happy path', async (t) => {
   const bootstrap = await setupTestnet(t)
   const { server: s1 } = await setupRpcServer(t, bootstrap)
   const { server: s2 } = await setupRpcServer(t, bootstrap)
@@ -20,29 +20,27 @@ test('happy path', async t => {
   t.is(pool.retries, 3, 'default retries')
   t.is(pool.timeout, 3000, 'default timeout')
 
-  const res = await pool.makeRequest(
-    'echo',
-    'hi',
-    { requestEncoding: cenc.string, responseEncoding: cenc.string }
-  )
+  const res = await pool.makeRequest('echo', 'hi', {
+    requestEncoding: cenc.string,
+    responseEncoding: cenc.string
+  })
   t.is(res, 'hi', 'rpc request processed successfully')
   t.is(rpcClient.nrConnections, 1, '1 connection opened')
   const initKey = pool.chosenKey
   t.is(initKey.byteLength, 32, 'sanity check to ensure it is a valid key')
 
   for (let i = 0; i < 10; i++) {
-    await pool.makeRequest(
-      'echo',
-      'hi',
-      { requestEncoding: cenc.string, responseEncoding: cenc.string }
-    )
+    await pool.makeRequest('echo', 'hi', {
+      requestEncoding: cenc.string,
+      responseEncoding: cenc.string
+    })
   }
 
   t.is(pool.chosenKey, initKey, 'keeps using the same key')
   t.is(rpcClient.nrConnections, 1, 'sanity check')
 })
 
-test('retries with other key if a server is unavailable', async t => {
+test('retries with other key if a server is unavailable', async (t) => {
   const bootstrap = await setupTestnet(t)
   const { server: s1, dht: dht1 } = await setupRpcServer(t, bootstrap)
   const { server: s2, dht: dht2 } = await setupRpcServer(t, bootstrap)
@@ -51,11 +49,10 @@ test('retries with other key if a server is unavailable', async t => {
   const pool = new Pool([s1.publicKey, s2.publicKey], rpcClient)
 
   {
-    const res = await pool.makeRequest(
-      'echo',
-      'hi',
-      { requestEncoding: cenc.string, responseEncoding: cenc.string }
-    )
+    const res = await pool.makeRequest('echo', 'hi', {
+      requestEncoding: cenc.string,
+      responseEncoding: cenc.string
+    })
     t.is(res, 'hi', 'rpc request processed successfully')
   }
 
@@ -71,18 +68,17 @@ test('retries with other key if a server is unavailable', async t => {
   }
 
   {
-    const res = await pool.makeRequest(
-      'echo',
-      'hi',
-      { requestEncoding: cenc.string, responseEncoding: cenc.string }
-    )
+    const res = await pool.makeRequest('echo', 'hi', {
+      requestEncoding: cenc.string,
+      responseEncoding: cenc.string
+    })
     t.is(res, 'hi', 'rpc request processed successfully')
   }
 
   t.alike(pool.chosenKey, choseS1 ? s2.publicKey : s1.publicKey, 'switched to the other server')
 })
 
-test('retries with other key if a request times out', async t => {
+test('retries with other key if a request times out', async (t) => {
   const bootstrap = await setupTestnet(t)
   const { server: s1, setDelay: setDelay1 } = await setupRpcServer(t, bootstrap)
   const { server: s2, setDelay: setDelay2 } = await setupRpcServer(t, bootstrap)
@@ -91,11 +87,10 @@ test('retries with other key if a request times out', async t => {
   const pool = new Pool([s1.publicKey, s2.publicKey], rpcClient, { timeout: 500 })
 
   {
-    const res = await pool.makeRequest(
-      'echo',
-      'hi',
-      { requestEncoding: cenc.string, responseEncoding: cenc.string }
-    )
+    const res = await pool.makeRequest('echo', 'hi', {
+      requestEncoding: cenc.string,
+      responseEncoding: cenc.string
+    })
     t.is(res, 'hi', 'rpc request processed successfully')
   }
 
@@ -109,60 +104,66 @@ test('retries with other key if a request times out', async t => {
   }
 
   {
-    const res = await pool.makeRequest(
-      'echo',
-      'hi',
-      { requestEncoding: cenc.string, responseEncoding: cenc.string }
-    )
+    const res = await pool.makeRequest('echo', 'hi', {
+      requestEncoding: cenc.string,
+      responseEncoding: cenc.string
+    })
     t.is(res, 'hi', 'rpc request processed successfully')
   }
 
   t.alike(pool.chosenKey, choseS1 ? s2.publicKey : s1.publicKey, 'switched to the other server')
 })
 
-test('Too-many-retries error if there are too many failed attempts', async t => {
+test('Too-many-retries error if there are too many failed attempts', async (t) => {
   const bootstrap = await setupTestnet(t)
   const rpcClient = getRpcClient(t, bootstrap)
 
-  const pool = new Pool(['a'.repeat(64), 'b'.repeat(64), 'c'.repeat(64), 'd'.repeat(64)], rpcClient, { timeout: 100 })
-
-  await t.exception(
-    async () => { await pool.makeRequest('echo', 'hi') },
-    'TOO_MANY_RETRIES:'
+  const pool = new Pool(
+    ['a'.repeat(64), 'b'.repeat(64), 'c'.repeat(64), 'd'.repeat(64)],
+    rpcClient,
+    { timeout: 100 }
   )
+
+  await t.exception(async () => {
+    await pool.makeRequest('echo', 'hi')
+  }, 'TOO_MANY_RETRIES:')
 })
 
-async function setupTestnet (t) {
+async function setupTestnet(t) {
   const testnet = await createTestnet()
-  t.teardown(async () => {
-    await testnet.destroy()
-  }, { order: 1000_000 })
+  t.teardown(
+    async () => {
+      await testnet.destroy()
+    },
+    { order: 1000_000 }
+  )
   return testnet.bootstrap
 }
 
-async function setupRpcServer (t, bootstrap, { msDelay = 0 } = {}) {
+async function setupRpcServer(t, bootstrap, { msDelay = 0 } = {}) {
   const dht = new HyperDHT({ bootstrap })
   const server = dht.createServer()
 
   let nrCons = 0
 
-  server.on('connection', conn => {
+  server.on('connection', (conn) => {
     if (DEBUG) {
       console.log('RPC connection received')
-      conn.on('close', () => { console.log('RPC connection closed') })
+      conn.on('close', () => {
+        console.log('RPC connection closed')
+      })
     }
     nrCons++
     const rpc = new ProtomuxRPC(conn, {
       id: server.publicKey,
       valueEncoding: cenc.none
-
     })
     rpc.respond(
       'echo',
       { requestEncoding: cenc.string, responseEncoding: cenc.string },
       async (req) => {
         if (msDelay > 0) {
-          await new Promise(resolve => {
+          await new Promise((resolve) => {
             const timeout = setTimeout(resolve, msDelay)
             dht.on('close', () => {
               clearTimeout(timeout)
@@ -171,25 +172,39 @@ async function setupRpcServer (t, bootstrap, { msDelay = 0 } = {}) {
           })
         }
         return req
-      })
+      }
+    )
   })
 
-  t.teardown(async () => {
-    await dht.destroy()
-  }, { order: 100 })
+  t.teardown(
+    async () => {
+      await dht.destroy()
+    },
+    { order: 100 }
+  )
 
   await server.listen()
-  return { server, getNrCons: () => nrCons, setDelay: (delay) => { msDelay = delay }, dht }
+  return {
+    server,
+    getNrCons: () => nrCons,
+    setDelay: (delay) => {
+      msDelay = delay
+    },
+    dht
+  }
 }
 
-function getRpcClient (t, bootstrap) {
+function getRpcClient(t, bootstrap) {
   const dht = new HyperDHT({ bootstrap })
   const rpc = new ProtomuxRpcClient(dht)
 
-  t.teardown(async () => {
-    await rpc.close()
-    await dht.destroy()
-  }, { order: 100 })
+  t.teardown(
+    async () => {
+      await rpc.close()
+      await dht.destroy()
+    },
+    { order: 100 }
+  )
 
   return rpc
 }
